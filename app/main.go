47ef03630671eff4e6f4194638674a76eb87f0eb
Arturo Vial Arqueros <tukovial@hotmail.com>
2023-10-18 00:28:09 -0300

    CORS fix

--- app/main.go
+++ app/main.go
@@ -74,7 +74,7 @@ func main() {
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
-		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
 	}))  
 
 	//initiate chatrooms
